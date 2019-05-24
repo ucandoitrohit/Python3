925870620f1aa0e1c6de01e3120af142a540f8e9
rohit <ucandoit.rohit@gmail.com>
2019-05-24 18:28:56 +0530

    Python Alexa

--- Automation/Alexa/Alexa_Speach_Recogination.py
+++ Automation/Alexa/Alexa_Speach_Recogination.py
@@ -3,10 +3,14 @@ import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
+import apiclient
 import os
 import sys
 from googlesearch import search
 from googlesearch import search_images
+from googleapiclient.discovery import build
+from google_images_download import google_images_download 
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -42,6 +46,8 @@ def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Please say something ! I am Listening to you.......")
+        speak("Please say something ! I am Listening to you.......")
+
         print(" ")
 
         r.pause_threshold = 1
@@ -49,14 +55,18 @@ def takeCommand():
 
     try:
         print("Recognizing your voice....... Please Wait......!")
+        speak("Recognizing your voice....... Please Wait......!")
+
         query = r.recognize_google(audio, language='en-in')
         print(" ")
-        print(f"You Said: {query}\n")
+        print(f"You Said:{query}\n")
         print(" ")
 
     except Exception as e:
         print(" ")
         print("Please say Again.... ! I did't Get you ? ")
+        speak("Please say Again....  ")
+        
         
         return "None"
     return query
@@ -68,25 +78,33 @@ def takeCommand():
 if __name__ == "__main__":
     speak("Welcome to Alexa World !")
 
-    wishMe()
+#    wishMe()
     while True:
 #if 1:
         query = takeCommand().lower()
+        keyword = query.replace("","+")
+        response = google_images_download.googleimagesdownload() 
+
 
         if 'wikipedia' in query:
             speak('Searching in Wikipedia.....')
             query = query.replace("wikipedia","")
             
             results = wikipedia.summary(query,sentences=2)
-            speak("I Found your query")
+            speak("I Found your query in wikipedia")
             print(results)
             speak(results)
+        
+      
 
         elif 'open youtube' in query:
             webbrowser.open("https://youtube.com")
 
         elif 'open google' in query:
             webbrowser.open("https://google.co.in")
+        
+        elif 'new tab' in query:
+            webbrowser.open_new_tab("https://google.co.in")
 
         elif 'open facebook' in query:
             webbrowser.open("https://facebook.com")
@@ -112,13 +130,23 @@ if __name__ == "__main__":
             
 
 
-        elif 'detail' in query:
+        elif 'open detail' in query:
             path = "D:\\NIC-Work\\MM.xlsx"
             os.startfile(path)
 
         elif 'open excel' in query:
             path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Excel 2013"
             os.startfile(path)
+
+        elif 'mailbox' in query:
+            path = "D:\\NIC-Work\\Mailboxs.xlsx"
+            os.startfile(path)
+
+        elif 'close mailbox' in query:
+            #path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Excel 2013"
+            path = "D:\\NIC-Work\\Mailboxs.xlsx"
+            #os.close('TASKKILL /IM excel.exe /F')
+            os.close( path)
 
         elif 'open word' in query:
             path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Word 2013"
@@ -132,9 +160,15 @@ if __name__ == "__main__":
             msgs1 = ['Hello! I am Fine ! How are you !']
             speak(msgs1)
 
+        elif  'facebook' in query:
+            for j in search(query, tld="co.in", num=10, stop=1, pause=2):
+             print("Your Url:" + j)
+             speak("Your Url:" + j)
 
-        elif 'close' in query or 'exit' in query or 'stop' in query:
-            speak("Good Bye")
+
+
+        elif 'close' in query or 'exit' in query or 'bye' in query:
+            speak("Good Bye ! Have a Nice day")
             sys.exit()
 
 
