6e8e487867f7568e9d3df2a5a957ddbf9f8f362c
rohit <ucandoit.rohit@gmail.com>
2021-02-09 18:31:28 +0530

    Check Which VM is down or up

---
+++ PowerShell Script/Check-VM-Status-PowerState.ps1
@@ -0,0 +1,16 @@
+VM is Down			
+			
+$vcentername = @("vcsdcxeu01.goep.hm.com")			
+$credential = Get-Credential			
+Connect-VIserver -Server $vcentername -Credential $credential -Force			
+$vms = Get-Content C:\Servername.txt			
+foreach($vm in $vms){			
+    $check = Get-VM $vm | Select Name, PowerState			
+    if($check.PowerState -like "PoweredOn"){			
+        Wriet-host "$vm is up"			
+    }			
+    else			
+    {			
+       Wriet-host "$vm is down"			
+    }			
+}			
