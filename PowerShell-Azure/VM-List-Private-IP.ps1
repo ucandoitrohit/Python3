df3f55227cb9b203d1f373e20fd7d37dd371aad1
rohit <ucandoit.rohit@gmail.com>
2023-01-11 10:49:21 +0530

    Get VM Detail with Dynamic IP

---
+++ PowerShell-Azure/VM-List-Private-IP.ps1
@@ -0,0 +1,19 @@
+#Get VM List With Dynamic Private IP Address
+# Get VM Name which has Dynamic IP from Multiple subscription
+
+Connect-AzAccount
+$subscriptionlist = Get-AzSubscription
+$outpath = "C:\temp\dynamiciplist.csv"
+"SubscriptionName,VirtualMachine,ResourceGroupName,NIC Name" | Out-File -FilePath $outpath
+foreach ($subscriptionlist1 in $subscriptionlist){
+    $subname = $subscriptionlist1.Name
+    Select-AzSubscription -Subscription $subname
+    $dynamiciplist = Get-AzNetworkInterface | Where-Object {$_.IpConfigurations.PrivateIpAllocationMethod -eq "Dynamic"}
+    foreach ($dynamiciplist1 in $dynamiciplist){
+        $nicname = $dynamiciplist1.Name
+        $resourcegroupname = $dynamiciplist1.ResourceGroupName
+        $virtualmachineid = (($dynamiciplist1.Id) -split "/virtualMachines/")[-1]
+        $virtualmachine = (($dynamiciplist1.Virtualmachine.Id) -split "/virtualMachines/")[-1]
+        "$subname,$virtualmachine,$resourcegroupname,$nicname" | Out-File -FilePath $outpath -Append
+    }
+}
