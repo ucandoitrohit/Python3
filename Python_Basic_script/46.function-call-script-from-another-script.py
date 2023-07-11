cb925b430287b53f76d974415a81dca061b41c84
rohit <ucandoit.rohit@gmail.com>
2023-07-11 10:02:59 +0530

    function call script

---
+++ Python_Basic_script/46.function-call-script-from-another-script.py
@@ -0,0 +1,11 @@
+# call a script from another script
+def addition(a,b):
+    print(f"Addition-{a}+{b}={a+b}")
+    return None
+
+
+def subtraction(a,b):
+    print(f"Addition-{a}-{b}={a-b}")
+    return None
+
+
