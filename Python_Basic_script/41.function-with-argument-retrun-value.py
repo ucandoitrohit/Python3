d5c542580e5c0450a7bb7e4cac199a90961f1d11
rohit <ucandoit.rohit@gmail.com>
2023-07-11 10:00:54 +0530

    function with argument

--- Python_Basic_script/41.function-with-argument-retrun-value.py
+++ Python_Basic_script/41.function-with-argument-retrun-value.py
@@ -13,7 +13,16 @@ def main():
     b=eval(input("Enter the second value-"))
     result=add(a,b)
     print(f"Addition {a} + {b} = { result }")
- 
     return None    
 
 main()
+#################################################
+def multiple(value):
+    return value*10
+
+def main2():
+    num2=eval(input("Enter the num: "))
+    result2=multiple(num2)
+    print(f"Output-",result2)
+
+main2()
